d4d810cb6782a77def9f63ec5735bad4c8b1ed66
christinekarimi <karimikim3@gmail.com>
2018-06-03 19:56:06 +0300

    Added a new item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { NewItemComponent } from './new-item/new-item.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewItemComponent,
+    
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      // {
+      //   path: 'new-user',
+      //   component: NewUserComponent
+      // },
+      // {
+      //   path: 'user-list',
+      //   component: UserListComponent
+      // }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
