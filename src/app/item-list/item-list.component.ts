f8c4a3856d44b0765342d41e696de52b2759d322
christinekarimi <karimikim3@gmail.com>
2018-06-05 09:20:09 +0300

    Added a modal to show details of a specific itam

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { Store } from '../store';
+import { NgbModal, ModalDismissReasons, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -12,9 +13,46 @@ export class ItemListComponent implements OnInit {
 
   items = Store;
 
-  constructor() { }
+  
+  constructor(private modalService: NgbModal,  private userService: UserService) {}
 
   ngOnInit() {
+    var single = this.userService.getData();
+    single.snapshotChanges().subscribe(item => {
+      this.userList = [];
+      item.forEach(element => {
+        var all = element.payload.toJSON();
+        all["$key"] = element.key; 
+        this.userList.push(all as User);
+      })
+    });
   }
 
+  userDetails(usr: User){
+    this.userService.selectedUser = usr;
+    this.firstname = usr.firstname;
+    this.lastname = usr.lastname;
+    this.rating = usr.rating;
+    this.category = usr.category;
+    this.subcategory = usr.subcategory;
+    this.amount = usr.amount;
+  }
+
+  open(content) {
+    this.modalService.open(content, { size: 'lg' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
